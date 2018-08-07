5aa62cb7c4777ec307606437739f92b6d71158bf
quhairfoir <moragstc@gmail.com>
2018-08-07 19:36:54 -0400

    added short url redirect to long url

--- express_server.js
+++ express_server.js
@@ -16,6 +16,12 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params.shortURL);
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls", (req, res) => { 
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
