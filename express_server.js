0f003c848d8bf34b949ce0685e802d34f0a3dfc8
quhairfoir <moragstc@gmail.com>
2018-08-07 17:07:16 -0400

    first commit, server up and running with urls_index and urls_show added

---
+++ express_server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => { 
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+})
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+    longURL: urlDatabase
+  };
+  res.render("urls_show", templateVars);
+});
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+});
