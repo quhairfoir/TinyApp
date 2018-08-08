d83bc2cc4f5cfa1e59bccc1b86ddc489cbad08c0
quhairfoir <moragstc@gmail.com>
2018-08-08 17:40:43 -0400

    400 status added to register route

--- express_server.js
+++ express_server.js
@@ -12,7 +12,36 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/register", (req, res) => {
+  if (!req.body.email || !req.body.password){
+    console.log(res);
+    res.status(400);
+    res.send("400: Email and password required!");
+  } else {
+  let newID = generateRandomString();
+  users[newID] = {};
+  users[newID].id = newID;
+  users[newID].email = req.body.email;
+  users[newID].password = req.body.password;
+  res.cookie("user_id", newID);
+  res.redirect("/urls");
+  }
+})
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -60,6 +89,13 @@ app.get("/urls/:id", (req, res) => {
     username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("register", templateVars);
 });
 
 app.post("/login", (req, res) => {
